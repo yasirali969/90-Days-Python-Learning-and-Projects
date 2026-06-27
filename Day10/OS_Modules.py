5964e5c95af0e9399df70174d0b812f849c0719f
yasirali969 <yasirali.bscssaif25@iba-suk.edu.pk>
2026-06-27 08:29:39 +0500

    Added Tkinter Projects folder

---
+++ Day10/OS_Modules.py
@@ -0,0 +1,20 @@
+import os
+
+# for i in range(10,100):
+#    os.mkdir(f"Day{i}")  # creating 90 folders
+
+# renaming the files
+# os.rename(f"Day{i}",f"Tutorial{i}")
+
+
+# Checking how many files in the folder
+'''folder=os.listdir("90 DAYS PYTHON")
+print(folder)'''
+
+
+# Printing the directory
+'''print(os.getcwd())
+os.chdir("//User")      # changing the directory to User(name)
+print(os.getcwd())'''
+
+
